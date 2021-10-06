40b7723d4defaf3ba50512b053ae0579ca964140
error-rgl <rglcnd@gmail.com>
2021-10-06 22:51:28 +0000

    function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * or 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
