10f82891c707380cb9193f300815621b1951db92
error-rgl <rglcnd@gmail.com>
2021-10-06 05:57:16 +0000

    function that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int j=1;
+	
+	while (j <= 10)
+	{
+		int i;
+		for(i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		
+		j++;
+	}
+}
