b3cb75b49be16c4179f5b28217b5b7c28bc47dc0
error-rgl <rglcnd@gmail.com>
2021-10-06 23:48:55 +0000

    function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		r = (i * -1);
+		return (r);
+	}
+	else
+	{
+		return (i);
+	}
+}
