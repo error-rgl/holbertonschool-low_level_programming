d13c6ae23dad7216dd16611f3685cb82b8e29124
error-rgl <rglcnd@gmail.com>
2021-10-07 01:08:00 +0000

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ *
+ * @a: number one.
+ * @b: number two.
+ *
+ * Return: Add of number one and number two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
