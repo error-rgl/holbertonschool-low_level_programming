9b58e43965012960ea9c71c361660ccf9d81853e
error-rgl <rglcnd@gmail.com>
2021-10-07 01:20:40 +0000

    function that prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: The first printed number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
+}
