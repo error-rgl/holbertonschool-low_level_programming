ca2d2738bb74be5ec2158c093fd175bfcd25cf6b
error-rgl <rglcnd@gmail.com>
2021-10-07 23:02:24 +0000

    Fixing file. Replacing spaces with tabs v2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,16 @@
 #include <stdlib.h>
-#include <time.h>
-/** more headers goes there
- *
- * betty style doc for function main goes there
+#include <main.h>
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
