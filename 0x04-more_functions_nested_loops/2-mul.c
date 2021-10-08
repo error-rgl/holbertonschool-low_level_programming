d8547d02dc81d0f39b270a34ed01f02901ea62cf
error-rgl <rglcnd@gmail.com>
2021-10-08 03:58:57 +0000

    Create 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie two integers
+ *
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
