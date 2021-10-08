f173ecaa8803d10ce61f228e0d15ee1f27ccda68
error-rgl <rglcnd@gmail.com>
2021-10-08 04:23:51 +0000

    Create 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - printsnumbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+
+	}
+	_putchar('\n');
+}
