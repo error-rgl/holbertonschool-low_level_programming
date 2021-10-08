ed56e9aefde3530ffa5c28cbbaf2a2257fa7e80e
error-rgl <rglcnd@gmail.com>
2021-10-08 04:27:54 +0000

    remove the if braces from line 14 and 16

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - printsnumbers between 0 to 9
+ * print_most_numbers - prints numbers between 0 to 9
  * (neither 2 nor 4).
+ *
  * Return: no return.
  */
 
@@ -13,10 +14,7 @@ void print_most_numbers(void)
 	for (i = 48; i < 58; i++)
 	{
 		if (i != 2 && i != 4)
-		{
 			_putchar(i);
-		}
-
 	}
 	_putchar('\n');
 }
