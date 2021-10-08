afbb43bf1ea3488a74f9b63528794796b99456a6
error-rgl <rglcnd@gmail.com>
2021-10-08 04:08:28 +0000

    Create 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ *
+ * Return: no return.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
