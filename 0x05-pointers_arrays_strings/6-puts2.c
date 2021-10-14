f2e48de760467e14a900bf9633537a38bea55e59
error-rgl <rglcnd@gmail.com>
2021-10-14 02:55:55 +0000

    Create 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every 2 characters of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (strt[count] == '\0')
+		{
+			_putchar ('\n');
+			break;
+		}
+
+		if (count % 2 == 0)
+		{
+			_putchar (str[count]);
+		}
+		count++;
+	}
+}
