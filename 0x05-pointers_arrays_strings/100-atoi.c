d66c40b8095618c38846f19f6b37486d23b0065c
error-rgl <rglcnd@gmail.com>
2021-10-14 04:09:31 +0000

    Create 100-atoi.c file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, d = 0, n = 0, count = 0, f = 0, digit = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	while (i < count && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
