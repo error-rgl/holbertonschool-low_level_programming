25dbf9dff2f456aaac0f4675e2c4cb42a4a11752
error-rgl <rglcnd@gmail.com>
2021-10-15 18:13:21 +0000

    Change tha line 15

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count = 0;
 
-	while (dest[count] != '\0' && count < n)
+	while ( count < n && dest[count] != '\0')
 	{
 		dest[count] = src[count];
 		count++;
