eb2507c75d366e5250bc0955a1b07d9b84609d57
error-rgl <rglcnd@gmail.com>
2021-10-15 18:05:04 +0000

    Create 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+
+	while (dest[count] != '\0' && count < n)
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	while (count < n)
+	{
+		dest[count] = '\0';
+		count++;
+	}
+}
