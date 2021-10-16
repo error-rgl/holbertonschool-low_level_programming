d3194dc2573acbd9b8c989077eb87b1a6325c31a
error-rgl <rglcnd@gmail.com>
2021-10-16 04:54:39 +0000

    Update file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - Prints an integer using _putchar function
+ * @n: Integer to print
+ * Return: void
  */
+
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	int power10 = 1, digit;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		n *= -1;
+		_putchar('-');
 	}
-	else
+	while (power10 * 10 <= n)
+		power10 *= 10;
+	while (power10 > 0)
 	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
+		digit = (n / power10) % 10;
+		power10 /= 10;
+		_putchar('0' + digit);
 	}
 }
