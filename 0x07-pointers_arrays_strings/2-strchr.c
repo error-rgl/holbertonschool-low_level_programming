b2fae86615892300d586215cf8c4321e9abc5c8e
error-rgl <rglcnd@gmail.com>
2021-10-19 04:55:56 +0000

    Create 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence
+ * of the cfharacter c.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
