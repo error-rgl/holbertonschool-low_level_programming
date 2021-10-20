2535a14bd2bdaf2d6ef89f7e8965d11288572b16
error-rgl <rglcnd@gmail.com>
2021-10-20 04:34:11 +0000

    [:Update:] content file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,13 +12,13 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				return (&s[i]);
+			if (*(s + i) == *(accept + j))
+				return (s + i);
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
