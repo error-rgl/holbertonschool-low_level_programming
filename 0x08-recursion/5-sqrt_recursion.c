b02f0b2fae05be4e33f12df0ca8ee0141333f017
error-rgl <rglcnd@gmail.com>
2021-10-21 04:23:21 +0000

    Change the variable num to n

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,21 @@
 
 /**
  * square_root - returns the natural square root of a number.
- * @num: input number.
+ * @n: input number.
  * @count: iterator.
  * Return: square root or -1.
  */
 
-int square_root(int count, int num)
+int square_root(int count, int n)
 {
-	if (count % (num / count) == 0)
+	if (count % (n / count) == 0)
 	{
-		if (count * (num / count) == num)
+		if (count * (n / count) == n)
 			return (count);
 		else
 			return (-1);
 	}
-	return (0 + square_root(num, count + 1));
+	return (0 + square_root(n, count + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number.
@@ -31,5 +31,5 @@ int _sqrt_recursion(int n)
 		return (0);
 	if (n == 1)
 		return (1);
-	return (square_root(num, 2));
+	return (square_root(n, 2));
 }
