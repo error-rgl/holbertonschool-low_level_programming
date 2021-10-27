fee6060cd9bb150ae8a2a79af5b82c75b9d86e6b
error-rgl <rglcnd@gmail.com>
2021-10-27 23:48:18 +0000

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *_strdup(char *str)
+{
+	char *cp;
+	unsigned int i, j;
+
+	if (cp == NULL)
+		return (NULL);
+
+	for (i = 0; cp[i] != '\0'; i++)
+		;
+
+	cp = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (cp == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		cp[j] = str[j];
+
+	return (cp);
+}
