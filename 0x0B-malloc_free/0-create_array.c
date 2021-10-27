950606addac4ebbc576e1e456b8dd6e2f591924a
error-rgl <rglcnd@gmail.com>
2021-10-27 23:38:42 +0000

    Craete 0-create_array.c file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *new;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	new = malloc(sizeof(c) * size);
+
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new[i] = c;
+
+	return (new);
+}
