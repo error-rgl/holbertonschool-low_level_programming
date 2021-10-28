39a2a0fce06cde20cf80a36d3ac6c28083afc06f
error-rgl <rglcnd@gmail.com>
2021-10-28 05:33:49 +0000

    Fixed content

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,34 +1,40 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * argstostr - concat all arguments
  * @ac: argument counter
  * @av: arguments 2D array
  * Return: new string
  */
+
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int row, col, count;
-	int index = 0;
+	int row, index = 0;
+	int size = 0;
+	unsigned int col, leng;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
+
 	for (row = 0; row < ac; row++)
 	{
-		for (col = 0; av[row][col] != '\0'; col++)
+		for (leng = 0; leng < strlen(av[row]); leng++)
 		{
-			count++;
+			size++;
 		}
 	}
-	str = malloc(sizeof(char) * (count + ac + 1));
+	str = malloc(sizeof(char) * size + ac);
 	if (str == NULL)
 		return (NULL);
 	for (row = 0; row < ac; row++)
 	{
-		for (col = 0; av[row][col] != '\0'; col++)
+		for (col = 0; col < strlen(av[row]); col++)
 		{
 			str[index] = av[row][col];
 			index++;
