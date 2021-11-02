b813be87c8d5a20ae251057fe507c5370ebcee3f
error-rgl <rglcnd@gmail.com>
2021-11-02 04:53:52 +0000

    Delete on line 16 and 17

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,11 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
-		;
-	else
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
