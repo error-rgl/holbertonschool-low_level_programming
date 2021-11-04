0f833da1f2311a0ef139bc106275f2db2dc373bb
error-rgl <rglcnd@gmail.com>
2021-11-04 17:44:32 +0000

    create 3-main.c file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code for holberton school students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		if ( get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+		return (1);
+	}
+	printf("Error\n");
+	exit(98);
+}
