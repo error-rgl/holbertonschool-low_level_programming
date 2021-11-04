f7d02aa5fc09d6cf32620a835cffe6e371063737
error-rgl <rglcnd@gmail.com>
2021-11-04 18:21:19 +0000

    Edit on line 15

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,13 +12,13 @@ int main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		if ( get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+		if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 		{
 			printf("Error\n");
 			exit(99);
 		}
 		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-		return (1);
+		return (0);
 	}
 	printf("Error\n");
 	exit(98);
