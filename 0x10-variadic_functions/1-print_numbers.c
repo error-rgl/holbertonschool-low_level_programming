c97645201f5e760a0437ccd7ec4cbeb1c5aa7c93
error-rgl <rglcnd@gmail.com>
2021-11-05 04:45:31 +0000

    Create 1-primnt_numbers.c file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ *
+ * Return: no return.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vlist;
+	unsigned int i;
+
+	va_start(vlist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(vlist, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(vlist);
+}
