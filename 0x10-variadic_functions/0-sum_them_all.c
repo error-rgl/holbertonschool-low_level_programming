628d50026b2bdc40621c58caaa19bf557068f7d2
error-rgl <rglcnd@gmail.com>
2021-11-05 04:34:06 +0000

    Create 0-sum_them_all.c file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ *
+ * Return: sum of its parameters.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list vlist;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(vlist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(vlist, int);
+
+	va_end(vlist);
+
+	return (sum);
+}
