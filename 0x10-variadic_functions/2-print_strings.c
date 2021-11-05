ba425906eae9b673462a7cf76fe39991a5a90cb3
error-rgl <rglcnd@gmail.com>
2021-11-05 04:55:47 +0000

    2-print_string.c file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings.
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
+ *
+ * Return: no return.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (i && separator)
+			printf("%s", separator);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+	}
+	printf("\n");
+	va_end(ap);
+}
