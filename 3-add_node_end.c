ae75cd21f006ee95c8fec619028b5d6eb53a75b7
error-rgl <rglcnd@gmail.com>
2021-11-12 21:36:07 +0100

    Create 3-add_node_end.c file

---
+++ 3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *head_temp;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		head_temp = *head;
+		while (head_temp->temp != NULL)
+			head_temp = head_temp->next;
+		head_temp->nest = new_node;
+	}
+	return (new_node);
+}
