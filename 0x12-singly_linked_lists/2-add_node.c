081f61101797ee1f4ebcb6697edba2b6f0cfd592
error-rgl <rglcnd@gmail.com>
2021-11-12 17:42:12 +0100

    [:Update:] Change all content

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,15 +9,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t new_node;
+	list_t *new_node;
+	size_t nchar;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == null)
-		return (null);
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
+	new_node->len = nchar;
 	new_node->next = *head;
 	*head = new_node;
+
 	return (*head);
 }
