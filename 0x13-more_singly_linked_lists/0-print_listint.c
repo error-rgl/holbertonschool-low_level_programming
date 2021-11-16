f278e4dbef0d0c5fa2f015bfc3d5243644d2901c
error-rgl <rglcnd@gmail.com>
2021-11-16 05:51:56 +0100

    add comment line 13 and change the condition in line 14

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
-	/*imprimimos el numero de nodos*/
-	if (h != NULL)
+	/*mientras que la cabecera de la lista sea*/
+	/*diferente a nulo, imprimimos el numero de nodos*/
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", h->n;
 		h = h->next;
 		num_nodes++;
 	}
