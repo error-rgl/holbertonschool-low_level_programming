3ae909b07996294184132ab1529b7fe6446c3e48
error-rgl <rglcnd@gmail.com>
2021-11-19 05:18:59 +0100

    [:Update:] change all content file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,28 +9,21 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, temp;
-	(void)temp;
+	listint_t *new, *temp;
 
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->next = NULL;
-	temp = *head;
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (*head);
 	}
-	else
-	{
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = new_node;
-	}
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	current->next = new_node;
 	return (*head);
 }
