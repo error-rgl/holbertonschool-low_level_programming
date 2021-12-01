85b3156cc0a80ecb8bfff6c6df624c6482b74463
error-rgl <rglcnd@gmail.com>
2021-12-01 17:52:12 +0100

    [:Update:] all content

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
-	unsigned int num = 0;
+	unsigned int num;
+	int i, base_two;
 
 	if (!b)
-		return(0);
-	for (i = 0; b[0]; i++)
+		return (0);
+
+	num = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+		;
+
+	for (i--, base_two = 1; i >= 0; i--, base_two *= 2)
 	{
-		if (b[i] != '0')
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
+
+		if (b[i] & 1)
+		{
+			num += base_two;
+		}
 	}
-	for (i = 0; b[0]; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
+
 	return (num);
 }
