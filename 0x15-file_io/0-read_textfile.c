883107f95614f67b8a82c4a9ecc6e3be6302dc19
error-rgl <rglcnd@gmail.com>
2021-12-03 20:03:33 +0100

    Create function that reads a text file and prints it to the POSIX  * standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_desc;
+	ssize_t num_bit;
+	char * buffer;
+
+	if (!filename)
+		return (0);
+
+	file_desc = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	else
+	{
+		buf = malloc(sizeof(char) * letters);
+		if (!buffer)
+			return (0);
+
+		num_bit = read(file_desc, buffer, letters);
+		num_write = write(STDOUT_FILENO, buf, num_bit);
+
+		close(file_desc);
+		free(buffer);
+	}
+	return (num_write);
+}
