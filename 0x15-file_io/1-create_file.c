fc4c55149e6dd5a447e8573cfc0403db6bc4775b
error-rgl <rglcnd@gmail.com>
2021-12-06 04:14:45 +0100

    Create 1-create_file.c file V2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,8 @@ int create_file(const char *filename, char *text_content)
 	ssize_t res_write;
 
 	if (!filename)
-		returnb (1);
+		return (1);
+
 	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_desc == -1)
