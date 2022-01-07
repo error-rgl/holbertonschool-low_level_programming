6f9afbcdee6f15cb6707aa28078d4594ed77eed1
error-rgl <rglcnd@gmail.com>
2022-01-07 05:59:28 +0100

    [:Update:] Fix all content of the file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,36 +1,43 @@
-#include "lists.h"
-
-/**
- * insert_dnodeint_at_index - inserts a new node at a given position
- *
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
- * Return: the address of the new node, or NULL if it failed
- */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp;
-	dlistint_t *new_nd;
+	dlistint_t *new;
+	dlistint_t *head;
 	unsigned int i;
 
-	if (!h)
-		return (NULL);
-	temp = *h;
-	if (temp)
-		for (i = 0; temp->next != NULL && i <= idx; i++)
-			temp = temp->next;
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+	}
 
-	new_nd = malloc(sizeof(dlistint_t));
-	if (new_nd == NULL)
-		return (NULL);
-
-	new_nd->n = n;
-	new_nd->prev = temp->prev;
-	new_nd->next = temp;
-
-	temp->prev = new_nd;
-	temp->next->prev = new_nd;
-
-	return (new_nd);
+	return (new);
 }
