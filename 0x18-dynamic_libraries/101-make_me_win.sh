d8411e3f8659baca75a3199ecfbc0fb502880fa1
error-rgl <rglcnd@gmail.com>
2022-01-27 04:13:43 +0100

    [:Fix:] Edit on line 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/error-rgl/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://raw.github.com/error-rgl/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libinject.so
+export LD_PRELOAD=/tmp/libinject.so
