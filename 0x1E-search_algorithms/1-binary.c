bede0da91e57eebdbf20e5befdf1dd1c36884055
error-rgl <rglcnd@gmail.com>
2022-10-31 14:14:35 -0500

    [Update]: align code betty style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,33 +9,34 @@
  * @value: value to search in
  * Return: index of the number
  */
+
 int recursive_search(int *array, size_t size, int value)
 {
-	size_t half = size / 2;
-	size_t i;
+    size_t half = size / 2;
+    size_t i;
 
-	if (array == NULL || size == 0)
-		return (-1);
+    if (array == NULL || size == 0)
+        return (-1);
 
-	printf("Searching in array");
+    printf("Searching in array");
 
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+    for (i = 0; i < size; i++)
+        printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
-	printf("\n");
+    printf("\n");
 
-	if (half && size % 2 == 0)
-		half--;
+    if (half && size % 2 == 0)
+        half--;
 
-	if (value == array[half])
-		return ((int)half);
+    if (value == array[half])
+        return ((int)half);
 
-	if (value < array[half])
-		return (recursive_search(array, half, value));
+    if (value < array[half])
+        return (recursive_search(array, half, value));
 
-	half++;
+    half++;
 
-	return (recursive_search(array + half, size - half, value) + half);
+    return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
@@ -47,14 +48,15 @@ int recursive_search(int *array, size_t size, int value)
  * @value: value to search in
  * Return: index of the number
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	int index;
+    int index;
 
-	index = recursive_search(array, size, value);
+    index = recursive_search(array, size, value);
 
-	if (index >= 0 && array[index] != value)
-		return (-1);
+    if (index >= 0 && array[index] != value)
+        return (-1);
 
-	return (index);
+    return (index);
 }
